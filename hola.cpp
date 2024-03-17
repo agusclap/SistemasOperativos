811c02145d8e358a7179fe582ff0c3d26a263f23
admin <agustin@ubuntu.myguest.virtualbox.org>
2024-03-17 16:04:11 -0300

    hola.cpp

---
+++ hola.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+    double num1=0 , num2=0 , resultado=0;
+    cout<<"Ingrese dos numeros"<<endl;
+    cout<<"numero 1 : "<<endl;
+    cin>>num1;
+    cout<<"numero 2 : " <<endl;
+    cin>>num2;
+    resultado = num1 + num2;
+    cout<<"El resultado de la suma es " <<resultado<<endl;
+    
+    return 0;
+
+}
